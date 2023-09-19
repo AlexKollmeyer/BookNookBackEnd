eb0ec14e5e52d909e6a47cf13c32881821ac5166
Jaron <Jaronleach98@gmail.com>
2023-09-19 09:50:20 -0400

    Worked on the Post endpoint for favorites controllers.

--- FullStackAuth_WebAPI/Controllers/FavoritesController.cs
+++ FullStackAuth_WebAPI/Controllers/FavoritesController.cs
@@ -1,4 +1,5 @@
 ﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -50,8 +51,39 @@ namespace FullStackAuth_WebAPI.Controllers
 
         // POST api/<FavoritesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Favorite data)
         {
+            try
+            {
+                // Retrieve the authenticated user's ID from the JWT token
+                string userId = User.FindFirstValue("id");
+
+                // If the user ID is null or empty, the user is not authenticated, so return a 401 unauthorized response
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized();
+                }
+
+                
+                data.UserId = userId;
+
+                
+                _context.Favorites.Add(data);
+                if (!ModelState.IsValid)
+                {
+                    
+                    return BadRequest(ModelState);
+                }
+                _context.SaveChanges();
+
+                // Return the newly created car as a 201 created response
+                return StatusCode(201, data);
+            }
+            catch (Exception ex)
+            {
+                // If an error occurs, return a 500 internal server error with the error message
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // PUT api/<FavoritesController>/5
