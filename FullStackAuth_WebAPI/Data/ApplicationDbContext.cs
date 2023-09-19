2caeaef007330265c23cc37ba4d0d211613ff291
Jaron <Jaronleach98@gmail.com>
2023-09-19 17:47:05 -0400

    Fixed bugs in Migration folder.

--- FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
+++ FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
@@ -1,16 +1,15 @@
 ﻿using FullStackAuth_WebAPI.Models;
 using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FullStackAuth_WebAPI.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public DbSet<Review> Reviews { get; set; }
 
         public DbSet<Car> Cars { get; set; }
-
-        public DbSet<User> Users { get; set; }
 
         public DbSet<Favorite> Favorites { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
