116461c433619d45cd50ccee53a4783a222357f6
Jaron <Jaronleach98@gmail.com>
2023-09-18 20:53:35 -0400

    Created the Favorites and reviews controllers.

--- FullStackAuth_WebAPI/Controllers/ReviewsController.cs
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -42,7 +42,6 @@ namespace FullStackAuth_WebAPI.Controllers
                 // Retrieve the authenticated user's ID from the JWT token
                 string userId = User.FindFirstValue("id");
 
-                // If the user ID is null or empty, the user is not authenticated, so return a 401 unauthorized response
                 if (string.IsNullOrEmpty(userId))
                 {
                     return Unauthorized();
@@ -51,21 +50,21 @@ namespace FullStackAuth_WebAPI.Controllers
                 // Set the car's owner ID  the authenticated user's ID we found earlier
                 data.BookId = userId;
 
-                // Add the car to the database and save changes
+                
                 _context.Reviews.Add(data);
                 if (!ModelState.IsValid)
                 {
-                    // If the car model state is invalid, return a 400 bad request response with the model state errors
+                    
                     return BadRequest(ModelState);
                 }
                 _context.SaveChanges();
 
-                // Return the newly created car as a 201 created response
+                
                 return StatusCode(201, data);
             }
             catch (Exception ex)
             {
-                // If an error occurs, return a 500 internal server error with the error message
+                
                 return StatusCode(500, ex.Message);
             }
         }
